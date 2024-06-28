9f4adba7f3e09fd518ecc16b5f9035808fbfcfb4
Patt Pootrakul <144429481+rymTooo@users.noreply.github.com>
2024-06-28 11:20:42 +0700

    commit the docker file and related stuff

--- deploy_kafka/entrypoint.sh
+++ deploy_kafka/entrypoint.sh
@@ -12,5 +12,7 @@ done
 # Create a topic
 kafka-topics --create --topic test-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
 
+kafka-topics --create --topic receive-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
+
 # Keep the container running
 wait
