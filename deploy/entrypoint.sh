c6b48f5570eebd57d9046823a6f933da10fc74d7
Patt Pootrakul <144429481+rymTooo@users.noreply.github.com>
2024-07-02 14:50:07 +0700

    <remove> remove feature of kafka to create topics in entrypoint, might have to add manually

--- deploy/entrypoint.sh
+++ deploy/entrypoint.sh
@@ -1,18 +1,13 @@
 #!/bin/bash
 
-# Start Kafka in the background
 /etc/confluent/docker/run &
 
-# Wait for Kafka to be ready
 echo "Waiting for Kafka to be ready..."
-while ! nc -z localhost 9092; do
+while ! nc -z kafka 29092; do
   sleep 1
 done
 
-# Create a topic
 kafka-topics --create --topic test-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
-
 kafka-topics --create --topic receive-topic --bootstrap-server localhost:9092 --partitions 1 --replication-factor 1
 
-# Keep the container running
 wait
--- local_app/NBpipeline.py
+++ local_app/NBpipeline.py
@@ -7,56 +7,48 @@ from mlflow.models import infer_signature
 import pickle
 
 
-# this part is the environmental var for using with mlflow-minio on docker
+# this part is the environmental var for connecting mlflow to minio 
 os.environ['AWS_ACCESS_KEY_ID'] = 'CWRUnvN2zh8rqE7pidsw'
 os.environ['AWS_SECRET_ACCESS_KEY'] = 'V8RfUWQlnB4QUa7rGHbvfHjhjLiOutRa8AZ9TPvy'
-# os.environ['MLFLOW_DEFAULT_ARTIFACT_ROOT'] = 'http://localhost://mlflow'
 os.environ['MLFLOW_S3_ENDPOINT_URL'] = 'http://localhost:9000'
 
+# this method is ONLY used for changing negative label from -1 >>> to 0
 def modify_label(label):
     if label == -1:
         label = 0
     return label
 
-dataset_path = "D:/Kafka_spark/resources/sample_data.csv"
-limit = 1000
-rd_state = 10
+dataset_path = "./resources/sample_data.csv"
+limit = 1000 # limit the training data to limit*2 size
+rd_state = 10 # random state variable for train test split
 
 dataset = pd.read_csv(dataset_path)
-# dataset = pd.concat([dataset.head(limit), dataset.tail(limit)])
-dataset['label'] = dataset['label'].apply(modify_label)
+# dataset = pd.concat([dataset.head(limit), dataset.tail(limit)]) # reduce the size of dataset to limit*2
+dataset['label'] = dataset['label'].apply(modify_label) # change negative label from -1 to 0
 
 
-X=dataset[['text']]
+X=dataset[['text']] # choose the column with content
 # print("this is type of X", type(X))
-y=dataset['label']
+y=dataset['label'] # y is of type list, NOT dataframe
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.1,random_state=rd_state)
 
 nb = NBmodel()
-nb.fit(X_train, y_train)
+nb.fit(X_train, y_train) # train the model
 
-predictions = nb.predict(context=None, model_input=X_train)
+predictions = nb.predict(context=None, model_input=X_train) # make prediction
 scores = nb.score(X_test['text'], y_test)
 
-#test result on sample data
-# data2 = pd.read_csv("D:/Sentiment_analysis_model/resources/sample_data.csv", encoding='utf-8')
-# data2 = data2[['text']]
-# print("prediction : ", nb.predict(context=None, model_input=data2)["result"])
-# prediction :  {'positive': 0.31885, 'neutral': 0.1471, 'negative': 0.53405}
-
-
-
-#sample for mlflow model signature
-sample_input = pd.DataFrame({"text":["this is a sample positive tweet", "and this is negative :("]})
-sample_prediction = nb.predict(context=None,model_input=sample_input)
-
-
+#---------------------------------------mlflow part-------------------------------------------
 # mlflow config
 tracking_uri = "http://localhost:5000" #use mlflow docker ip address
 experiment_name = "NaiveBayes Sentiment analysis"
 run_name = "nb model"
 artifact_path = "NB_sentiment_analysis_model"
+
+#sample for mlflow model signature
+sample_input = pd.DataFrame({"text":["this is a sample positive tweet", "and this is negative :("]})
+sample_prediction = nb.predict(context=None,model_input=sample_input)
 
 
 # Define the model hyperparameters
@@ -64,15 +56,15 @@ params = {
     "datasize": limit*2,
     "random_state": rd_state,
     "logprior":nb.logprior,
-    # "loglikelihood":nb.loglikelihood >>> loglikelihood
+    # "loglikelihood":nb.loglikelihood # loglikelihood is too large to store as params
 }
-# print(nb.loglikelihood)
 
+# save these 2 weight to pickle format. Will later save to model artifacts
 with open("logprior.pkl", "wb") as f:
     pickle.dump(nb.logprior, f)
-
 with open("loglikelihood.pkl", "wb") as f:
     pickle.dump(nb.loglikelihood, f)
+
 
 mlflow.set_tracking_uri(uri=tracking_uri)
 
@@ -85,11 +77,11 @@ with mlflow.start_run(run_name=run_name):
     print("trakcing > ", mlflow.get_tracking_uri())
     print("artifact > ", mlflow.get_artifact_uri())
 
-    # Log the hyperparameters
+    # Log the hyperparameters >> for storing model information
     mlflow.log_params(params)
     print("log param success")
 
-    # Log the loss metric
+    # Log the loss metric >> for model evaluation
     mlflow.log_metrics(scores)
     print("log metrics success")
 
@@ -97,11 +89,8 @@ with mlflow.start_run(run_name=run_name):
     mlflow.set_tag("Training Info", "Naive Bayes for sentiment analysis")
 
     # Infer the model signature
-    signature = infer_signature(model_input=sample_input, model_output=sample_prediction) # to be fix in a more specific format
+    signature = infer_signature(model_input=sample_input, model_output=sample_prediction)
     print("finish creating signature")
-
-    # with open("save_model/python_model.pkl", 'wb') as file:
-    #     pickle.dump(nb, file)
 
     model_info = mlflow.pyfunc.log_model(
         python_model = NBmodel(),
