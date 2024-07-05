2efccd5b92bc7cd707402690da2a5f3222e30ff2
Patt Pootrakul <144429481+rymTooo@users.noreply.github.com>
2024-07-05 10:42:00 +0700

    <feature> add spark job for handling testing feature

--- app/spark_job_test.py
+++ app/spark_job_test.py
@@ -11,6 +11,7 @@ from pyspark.sql.functions import from_json
 import json
 from dotenv import load_dotenv
 
+
 # might need to change localhost to something else
 spark = SparkSession \
     .builder \
@@ -25,8 +26,8 @@ server_ip = os.getenv('SERVER_IP')
 mlflow_port = os.getenv('MLFLOW_PORT')
 
 #define kafka topic
-input_topic = "raw-data-topic"
-output_topic = "prediction-topic"
+input_topic = "test-raw-data-topic"
+output_topic = "test-prediction-topic"
 
 
 nltk.download('stopwords')
@@ -34,7 +35,6 @@ nltk.download('stopwords')
 # os.environ['AWS_ACCESS_KEY_ID'] = 'CWRUnvN2zh8rqE7pidsw'
 # os.environ['AWS_SECRET_ACCESS_KEY'] = 'V8RfUWQlnB4QUa7rGHbvfHjhjLiOutRa8AZ9TPvy'
 # os.environ['MLFLOW_S3_ENDPOINT_URL'] = 'http://s3:9000' #minio api ip:port
-
 # Set the tracking URI to your MLflow server
 mlflow.set_tracking_uri(f"http://{server_ip}:{mlflow_port}")  # Replace with your MLflow server URI
 
@@ -42,6 +42,7 @@ mlflow.set_tracking_uri(f"http://{server_ip}:{mlflow_port}")  # Replace with you
 model_name = os.getenv("REGISTERED_MODEL_NAME")
 model_version = int(os.getenv("REGISTERED_MODEL_VERSION"))
 model_uri = f"models:/{model_name}/{model_version}"  # Replace with your model name and version
+
 
 loaded_model = mlflow.pyfunc.load_model(model_uri)
 
@@ -58,27 +59,29 @@ object_name = "1/4c11f07b46654cda88b3840d9f655732/artifacts/NB_sentiment_analysi
 download_path = "/model/python_model.pkl"
 client.fget_object(bucket_name, object_name, download_path)
 print(f"File '{object_name}' downloaded successfully to '{download_path}'.")
+
+
 """
 
 results = []
+# input format {"snippet" : "message"}
 
 def process_row(row):
     if row == None or len(row) <= 0:
         print(row)
         print("exit ---------------------")
         return 0
-    df = pandas.DataFrame(json.loads(row[0]['value_string']))  
+    df = pandas.DataFrame(json.loads(row[0]['value_string']))
     df['text'] = df['snippet']
     print(df)
     predictions = loaded_model.predict(df[['text']])
     print(predictions)
-    # results.append({"text": predictions['text'], "label": predictions['label']})
-    results.append({"value": predictions.to_json(orient='records'), "key":df['query'][0]})
+    results.append({"value": predictions.to_json(orient='records')})
     
     predictions_df = spark.createDataFrame(results)
     predictions_df.printSchema()
     # Write predictions to Kafka
-    predictions_df.selectExpr("CAST(value AS STRING)", "CAST(key AS STRING)") \
+    predictions_df.selectExpr("CAST(value AS STRING)") \
         .write \
         .format("kafka") \
         .option("kafka.bootstrap.servers", "kafka:29092") \
@@ -96,7 +99,7 @@ df = spark \
   .option("kafka.bootstrap.servers", "kafka:29092") \
   .option("subscribe", input_topic) \
   .load()
-df.selectExpr("CAST(key AS STRING)", "CAST(value AS STRING)")
+df.selectExpr("CAST(value AS STRING)")
 df = df.withColumn("value_string", col("value").cast("string"))
 df.printSchema()
 
