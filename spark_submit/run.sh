208eab11f8b3ff45feeeae095ad27fb6b30d821c
Patt Pootrakul <144429481+rymTooo@users.noreply.github.com>
2024-07-11 13:51:52 +0700

    <modify> still doesn't work

---
+++ spark_submit/run.sh
@@ -0,0 +1,6 @@
+spark-submit --master spark://localhost:27077 \
+--deploy-mode client \
+--packages org.apache.spark:spark-sql-kafka-0-10_2.12:3.1.1 \
+--repositories https://repo1.maven.org/maven2/ \
+--files /opt/spark-job/.env \
+/opt/spark-job/spark_job.py
