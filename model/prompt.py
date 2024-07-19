27e706e5630ebdae0b6cb23e812bbbbad0c6094e
Patt Pootrakul <144429481+rymTooo@users.noreply.github.com>
2024-07-19 12:49:43 +0700

    <feature> now using ollama to call api when using model (typhoon)

--- model/prompt.py
+++ model/prompt.py
@@ -1,40 +1,41 @@
 
 # fixing unicode error in google colab
-import locale
+# import locale
 
-locale.getpreferredencoding = lambda: "UTF-8"
+# locale.getpreferredencoding = lambda: "UTF-8"
 
-# Import dependencies
-import torch
-from transformers import (
-    AutoModelForCausalLM,
-    AutoTokenizer,
-    BitsAndBytesConfig,
-    pipeline,
-)
 
-from langchain_core.prompts import PromptTemplate
+
 from langchain_core.prompts import ChatPromptTemplate
 from langchain_community.llms.ollama import Ollama
 template = ChatPromptTemplate.from_messages([
-    ("system", "Persona : You are a helpful AI that knows everything include expert knowledge. Your name is {name} and you are girl."),
-    ("system", "Task : Your task is to assist with  answer all question and give all detailed about it."),
-    ("system", "Context: You have access to all of knowledge on all thing."),
-    # ("system", "Format: Answer Thai languague only"),
-    ("human", "{user_input}"),
-    ("ai", "Response:"),
+    ("system", "You are a good assistant named Typhoon."),
+    ("system", "Task : Your task is to perform sentiment analysis on user input."),
+    ("system", "Format: Your answer must be in the format of {{'sentiment':[sentiment], 'confident_score':[score]}}."),
+    ("system", "Value for [sentiment] must be a string of either 'good', 'bad', or 'neutral' only."),
+    ("system", "Value for [score] must be a floating point between 1 and 0. confident score 1 mean the most confident and 0 mean the least confident."),
+    ("user", "โจรวิ่งหนีตำรวจลงคลอง"),
+    ("assistant", "{{'sentiment' : 'bad', 'confident_score' : 0.95}}"),
+    ("user", "ป่าไม้ในประเทศไทย"),
+    ("assistant", "{{'sentiment' : 'neutral', 'confident_score' : '0.85'}}"),
+    ("user", "ไฟไหม้ตึกร้างบริเวณหลังโรงเรียนอนุบาล"),
+    ("assistant", "{{'sentiment' : 'bad', 'confident_score' : 0.99}}"),
+    ("user", "ประเทศไทยชนะรางวัลกีฬาแบดมินตัน"),
+    ("assistant", "{{'sentiment' : 'good', 'confident_score' : 0.95}}"),
+    ("user", "อยากติด #จุฬา ต้องมูที่ไหน✨ . แนะนำที่พระบรมราชานุสาวรีย์สองรัชกาล สถานที่ศักดิ์สิทธิ์ของชาวจุฬา เหมาะกับคนอยากติดจุฬาสุด ๆ"),
+    ("assistant", "{{'sentiment' : 'good', 'confident_score' : 0.99}}"),
+    ("user", "{user_input}")
 ])
 llm = Ollama(
-    base_url = "http://192.168.123.110:31644",
-    model="qwen2:72b",
+    base_url = "http://192.168.123.110:11434",
+    model="llama3-typhoon-8b:latest",
     temperature=0.1,  # Adjust temperature for diversity
 ) 
-def generate_response(name, user_input):
+def generate_response(user_input):
     try:
         # Generate the prompt value with context and task
         prompt_value = template.invoke(
             {
-                "name": name,
                 "user_input": user_input
             }
         )
@@ -51,7 +52,8 @@ def generate_response(name, user_input):
         except Exception as e:
             print(f"Error during LLM invocation: {e}")
             return None
-name = "Malenia"
-user_input = "Did you know about resin?"
-response = generate_response(name, user_input)
+        
+
+user_input = "หลักสูตรแพทย์อินเตอร์ 4 ปี แห่งแรกในไทย"
+response = generate_response(user_input)
 print(response)
